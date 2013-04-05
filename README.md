e54459e0cf1692f860fe0665300b44083d24a2e6
Mike Scalora <mike@scalora.org>
2013-04-04 21:17:55 -0600

    added support for HEAD requests and If-Modified-Since header

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ cdpf-server
 --Coming Soon-- The server side software for running your own CDPF site for supplying photos to you own CDPF
 
 Other resources are available at: http://tinyurl.com/pi-cdpf
+
+Test: 4
--- php/index.php
+++ php/index.php
@@ -28,14 +28,27 @@
 				header ("HTTP/1.0 404 Not Found");
 				exit;
 			}
+
+			// Checking if the client is validating his cache and if it is current.
+			if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == filemtime($file))) {
+				// Client's cache IS current, so we just respond '304 Not Modified'.
+				header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($file)).' GMT', true, 304);
+				exit;
+			}
+			
+			header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($file)).' GMT', true, 200);
+			header('Content-Length: '.filesize($file));
+			// for HEAD requests (which WGET claims to use) we don't send the content
+			if ($_SERVER['REQUEST_METHOD']=='HEAD') {
+				exit;
+			}
+			
 			$seconds_to_cache = 60*60*24*365*5;
 			$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 			header("Expires: $ts");
 			header("Pragma: cache");
 			header("Cache-Control: max-age=$seconds_to_cache");
 			header('Pragma: public');
-			ob_clean();
-			flush();
 			readfile($file);
 			exit;
 			
