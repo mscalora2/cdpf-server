70d4e44280a242bfd04a4b171d9c974feafe987c
Mike Scalora <mike@scalora.org>
2013-04-06 23:02:09 -0600

    added new styles, delete, passwords, etc

---
+++ php/auth.php
@@ -0,0 +1,65 @@
+<?php
+	$authed = false;
+	$pwfile = "$dataDir.passwd";
+
+	if (!is_file("$pwfile")) {	
+		if (isset($_REQUEST['setup']) && isset($_REQUEST['secret']) && strlen($_REQUEST['secret'])>=6) {
+			$HASH = crypt($_REQUEST['secret']);
+			file_put_contents($pwfile,$HASH);
+			header('Location: /');
+			exit;
+		}
+		
+		if ($_SERVER["REQUEST_METHOD"]!="GET") {
+			header('Location: /');
+			exit;		
+		}
+		
+		echo $twig->render('auth.twig', array(
+			'title' => $title,
+			'prompt' => "Enter password of at least 6 characters to secure your site.",
+			'setup' => true
+		));
+		exit;
+	}
+
+	$UUID = isset($_ENV['OPENSHIFT_GEAR_UUID']) ? $_ENV['OPENSHIFT_GEAR_UUID'] : $_SERVER["SERVER_SIGNATURE"];
+	$DATE = date("dlFmY");
+	$IP = $_SERVER["REMOTE_ADDR"];
+	$PWHASH = file_get_contents($pwfile);
+	
+	$TOKEN = sha1("$UUID.$DATE.$IP.$PWHASH");
+	
+
+	if (isset($_REQUEST['logout'])) {
+		setcookie("ticket", "", time()-60*60*24, "/");
+		header('Location: /');
+		exit;
+	} elseif (isset($_REQUEST['login']) && isset($_REQUEST['secret'])) {
+		if (crypt($_REQUEST['secret'],$PWHASH)==$PWHASH) {
+			setcookie("ticket", $TOKEN, time()+60*60*24, "/");
+			header('Location: /');
+			exit;
+		}
+	} elseif(isset($_COOKIE['ticket'])) {
+		if ($_COOKIE['ticket']==$TOKEN) {
+			$authed = true;
+		}
+	}
+
+	if (!$authed) {
+		if ($_SERVER["REQUEST_METHOD"]!="GET") {
+			header('Location: /');
+			exit;		
+		}
+		
+		echo $twig->render('auth.twig', array(
+			'title' => $title,
+			'prompt' => "Login",
+			'setup' => false
+		));
+		exit;		
+	}
+
+
+?>
--- php/index.php
+++ php/index.php
@@ -1,22 +1,32 @@
 <?php
-
 	$dataDir = isset($_ENV['OPENSHIFT_DATA_DIR']) ? $_ENV['OPENSHIFT_DATA_DIR'] : (isset($_SERVER['OPENSHIFT_DATA_DIR']) ? $_SERVER['OPENSHIFT_DATA_DIR'] : '/var/www/data/');
-	$upload = !isset($_REQUEST['list']);
-  
+	$fromPi = isset($_REQUEST['list']) && count($_REQUEST)==1;
+	$title = "Connected Digital Photo Frame";
+
+	require_once 'twig/lib/Twig/Autoloader.php';
+	Twig_Autoloader::register();
+	$twig = new Twig_Environment(new Twig_Loader_Filesystem('.'));
+	
+	if (!$fromPi) {
+		require("auth.php");
+	}
+	
+	// test for image file names
 	function isImageName($name) {
 		return preg_match('/^[^.\/][^\/]*\.(jpe?g|png|gif)$/i',$name)>0;
 	}
 
-		/* handle image delivery */
+	// for debugging
 	if (isset($_REQUEST['phpinfo'])) {
 		phpinfo();
 		exit;
 	}
 	
-	/* handle image delivery */
+	// handle serving image files 
 	if (isset($_REQUEST['f'])) {
 		if (isImageName($_REQUEST['f']) && is_file($dataDir . $_REQUEST['f'])) {
 	
+			// set content type if a legal image extention 
 			$file = $dataDir . $_REQUEST['f'];
 			if (preg_match('/\.gif/i',$file)) {
 				header('Content-Type: image/gif');
@@ -36,6 +46,7 @@
 				exit;
 			}
 			
+			// insert Last-Modified header for wget's -N option
 			header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($file)).' GMT', true, 200);
 			header('Content-Length: '.filesize($file));
 			// for HEAD requests (which WGET claims to use) we don't send the content
@@ -43,25 +54,26 @@
 				exit;
 			}
 			
+			// tell clients to cache for 5 years
 			$seconds_to_cache = 60*60*24*365*5;
 			$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 			header("Expires: $ts");
 			header("Pragma: cache");
 			header("Cache-Control: max-age=$seconds_to_cache");
 			header('Pragma: public');
+			
+			// send image to client
 			readfile($file);
 			exit;
 			
 		}
+		
+		// error
 		header ("HTTP/1.0 404 Not Found");
 		exit;
 	}
-
-	require_once 'twig/lib/Twig/Autoloader.php';
-	Twig_Autoloader::register();
-	$twig = new Twig_Environment(new Twig_Loader_Filesystem('.'));
 	
-	$message = "Upload Photos To CDPF";
+	$message = "";
 	
 	if (isset($_REQUEST['submit']) && isset($_FILES["the-file"])) {
 	
@@ -71,20 +83,30 @@
 		if (isset($file["name"]) && isImageName($file["name"])) {
 		
 			$name = preg_replace('/^[.]|[^\w.]/i','-', $file['name']);
-			
 			$result = move_uploaded_file($file["tmp_name"],$dataDir.$name);
-			
 			$message = $result ? "The photo $name has been uploaded" :
 				"Error receiving the file: $name";
 		}
+		header('Location: /');
+		exit;
 	}
 
 	$images = array_filter(scandir($dataDir),'isImageName');
+
+	if (isset($_REQUEST['delete-image'])) {
+		$fileName = $_REQUEST['delete-image'];
+		if (in_array($fileName,$images,true)) {
+			unlink($dataDir.$fileName);
+			header('Location: /');
+			exit;
+		}
+	}
 	
-	echo $twig->render('index.twig', array(
+	echo $twig->render($fromPi ? 'list.twig' : 'index.twig', array(
+		'title' => $title,
 		'message' => $message,
 		'images' => $images,
-		'upload' => $upload
+		'ui' => !$fromPi
 	));
 
 ?>
